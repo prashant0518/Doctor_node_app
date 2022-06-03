ce3bdf221cca96d57f924ee76c241573ad9ccdb6
SSK <sajid.khan@team.cliffex.com>
2022-06-03 19:27:13 +0530

    update db

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -4,6 +4,13 @@ console.log('DB host ' + process.env.DB_HOST)
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PSWD, {
     dialect: 'postgres',
     host: process.env.DB_HOST,
+    dialectOptions: {
+        useUTC: false, // for reading from database
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
     // logging: false,
 
 });
